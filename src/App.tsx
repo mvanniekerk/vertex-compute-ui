2bc11b18a8aa4f807ef766b65f9f2f0b57a568f8
Matthijs van Niekerk <matthijsvanniekerk@Matthijss-MacBook-Pro.local>
2021-09-16 15:10:03 +0200

    Setu up main grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="graph"> 
+        Graph goes here.
+      </div>
+      <div className="console"> 
+        Console goes here.
+      </div>
+      <div className="infobar">
+        <div className="vertex-info">Vertex Info</div>
+        <div className="new-vertex">
+          <button id="new-vertex">New Vertex</button>
+        </div>
+      </div>
     </div>
   );
 }
